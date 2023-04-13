1a0430512c0b197fc9471d10d5fb57b0685a93e8
eimwe <prokopenkoserge@inbox.ru>
2023-04-13 15:38:54 +0600

    Set basic style rules for the whole project
    1. Created Sass file architecture based on '7-1' approach;
    2. Included css custom variables, fonts and general style rules;
    3. Added test style rules for 'TheWelcome.vue' component;
    4. Imported 'main.scss' to the 'main.js' file;
    5. Deleted 'main.css' and 'base.css' stylesheets.

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -8,6 +8,11 @@ import SupportIcon from './icons/IconSupport.vue'
 </script>
 
 <template>
+  <h1 class="welcome__h1">h1</h1>
+  <h2 class="welcome__h2">h2</h2>
+  <h3 class="welcome__h3">h3</h3>
+  <p class="welcome__content">content</p>
+  <span class="welcome__note">note</span>
   <WelcomeItem>
     <template #icon>
       <DocumentationIcon />
