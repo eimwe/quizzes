16b929706784941aef07f246ca64a94a2cd06608
eimwe <prokopenkoserge@inbox.ru>
2023-04-19 16:13:07 +0600

    Updated routes with 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import CategoryView from '../views/CategoryView.vue'
+import NotFound from '../views/404View.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/category/:id',
       name: 'category',
       component: CategoryView
+    },
+    {
+      path: '/:catchall(.*)*',
+      name: '404',
+      component: NotFound
     }
   ]
 })
