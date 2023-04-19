246a1aa7cf51250e9d04d9140b66ddca06c0c656
eimwe <prokopenkoserge@inbox.ru>
2023-04-19 13:05:58 +0600

    Added the new end game component
    It shows the total amount of questions and correct answers passed as props

---
+++ src/components/Result.vue
@@ -0,0 +1,20 @@
+<script setup>
+  import { defineProps } from 'vue';
+  import { RouterLink } from 'vue-router';
+
+  const {
+    numberOfCorrectAnswers,
+    questionsLength
+    } = defineProps([
+     'numberOfCorrectAnswers',
+     'questionsLength'
+    ]);
+</script>
+
+<template>
+  <div class="container">
+    <p>Your results:</p>
+    <strong>{{ numberOfCorrectAnswers }}/{{ questionsLength }}</strong>
+    <RouterLink to="/">Return to the Home page</RouterLink>
+  </div>
+</template>
