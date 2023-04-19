8d631a8a81239ef44c087bcfff18022accc6e659
eimwe <prokopenkoserge@inbox.ru>
2023-04-19 13:07:23 +0600

    Rendered the results by the end of the game
    1. Passed the number of correct answers as well as the total amount of questions as props to the 'Result.vue' component;
    2. Added condition to the 'onOptionSelected' function to render the 'Result.vue' component.

--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -4,11 +4,13 @@
   import quizData from '../data/quizzes.json'
   import Question from '../components/Question.vue';
   import QuestionHeader from '../components/QuestionHeader.vue';
+  import Result from '../components/Result.vue';
 
   const categoryRoute = useRoute();
   const category = quizData.find(category => category.id === parseInt(categoryRoute.params.id));
   const currentQuestionIndex = ref(0);
   const numberOfCorrectAnswers = ref(0);
+  const showResults = ref(false);
 
   const quizProgress = computed(() => {
     return `${currentQuestionIndex.value}/${category.questions.length}`;
@@ -23,6 +25,10 @@
       numberOfCorrectAnswers.value++;
     }
 
+    if ((category.questions.length - 1)  === currentQuestionIndex.value) {
+      showResults.value = true;
+    }
+
     currentQuestionIndex.value++;
   }
 </script>
@@ -30,7 +36,13 @@
 <template>
   <QuestionHeader :quizProgress="quizProgress" :barProgress="barProgress"/>
   <Question
+    v-if="!showResults"
     :question="category.questions[currentQuestionIndex]"
     @selectedOption="onOptionSelected"
+  />
+  <Result
+    v-else
+    :numberOfCorrectAnswers="numberOfCorrectAnswers"
+    :questionsLength="category.questions.length"
   />
 </template>
