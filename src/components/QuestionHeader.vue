66b30abe4a3b0f86f2118e783f9bce4d975b0b73
eimwe <prokopenkoserge@inbox.ru>
2023-04-19 10:58:54 +0600

    Extracted question header into a separate component

---
+++ src/components/QuestionHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="page-header">
+    <div class="container">
+      <div class="page-header__container">
+        <strong class="page-header__title">Question 1/3</strong>
+        <label class="visually-hidden" for="progress-bar">Questions answered</label>
+        <progress id="progress-bar" class="progress" value="33" max="100">
+          <span class="visually-hidden">33%</span>
+        </progress>
+      </div>
+    </div>
+  </header>
+</template>
--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -2,22 +2,13 @@
   import { useRoute } from 'vue-router';
   import quizData from '../data/quizzes.json'
   import Question from '../components/Question.vue';
+  import QuestionHeader from '../components/QuestionHeader.vue';
 
   const categoryRoute = useRoute();
   const category = quizData.find(category => category.id === parseInt(categoryRoute.params.id));
 </script>
 
 <template>
-  <header class="page-header">
-    <div class="container">
-      <div class="page-header__container">
-        <strong class="page-header__title">Question 1/3</strong>
-        <label class="visually-hidden" for="progress-bar">Questions answered</label>
-        <progress id="progress-bar" class="progress" value="33" max="100">
-          <span class="visually-hidden">33%</span>
-        </progress>
-      </div>
-    </div>
-  </header>
+  <QuestionHeader />
   <Question :category="category"/>
 </template>
