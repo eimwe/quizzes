1b586c2eeac976fc9fa2ee36c2ee8ba0db7b9b99
eimwe <prokopenkoserge@inbox.ru>
2023-04-19 16:07:02 +0600

    Added 404 error page component

---
+++ src/views/404View.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import { RouterLink } from 'vue-router';
+  import coverImage from '../assets/images/no-cover.jpg';
+</script>
+
+<template>
+  <figure class="deadend">
+    <img class="deadend__image" :src="coverImage" alt="Not Found">
+    <figcaption>
+      <span class="deadend__title">404 You got lost</span>
+      <RouterLink to="/" class="link">Return to the Home page</RouterLink>
+    </figcaption>
+  </figure>
+</template>
