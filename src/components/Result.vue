c4674efc67e729bf15b33d37bd9511b64aab87dc
eimwe <prokopenkoserge@inbox.ru>
2023-04-22 12:28:56 +0600

    Updated 'Result.vue' component markup

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -19,26 +19,32 @@
     <div class="container">
       <p class="results__intro">Your results:</p>
       <strong class="results__total">{{ numberOfCorrectAnswers }}/{{ questionsLength }}</strong>
-      <table>
-        <caption>Answer keys:</caption>
-        <tr>
-          <th>#</th>
-          <th>Your variant</th>
-          <th>Correct answer</th>
-        </tr>
-        <tr v-for="question in questions" :key="question.id">
-          <td>{{ question.id }}</td>
-          <td
-            v-for="answer in answers"
-            :key="answer.id"
-            v-show="answer.id === question.id"
-          >{{ answer.label }}</td>
-          <td
-            v-for="option in question.options"
-            :key="option.id"
-            v-show="option.isCorrect"
-          >{{ option.label }}</td>
-        </tr>
+      <table class="chart results__key">
+        <caption class="chart__title">Answer keys:</caption>
+        <thead>
+          <tr>
+            <th class="chart__column chart__header">#</th>
+            <th class="chart__column chart__header">Your answer</th>
+            <th class="chart__column chart__header">Correct answer</th>
+          </tr>
+        </thead>
+        <tbody class="chart__content">
+          <tr v-for="question in questions" :key="question.id" class="chart__line">
+            <td class="chart__column">{{ question.id }}</td>
+            <td
+              v-for="answer in answers"
+              :key="answer.id"
+              v-show="answer.id === question.id"
+              class="chart__column"
+            >{{ answer.label }}</td>
+            <td
+              v-for="option in question.options"
+              :key="option.id"
+              v-show="option.isCorrect"
+              class="chart__column"
+            >{{ option.label }}</td>
+          </tr>
+        </tbody>
       </table>
       <RouterLink to="/" class="link">Return to the Home page</RouterLink>
     </div>
