4fbe0c6522127f158caa43c64cded8af26e5b9ff
eimwe <prokopenkoserge@inbox.ru>
2023-04-22 13:53:35 +0600

    Changed cover image for the card
    1. Added the new cover image to the 'assets/images' folder;
    2. Deleted condition from the image src;
    3. Added the new cover image as a background to the image container.

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,14 +1,14 @@
 <script setup>
   import { RouterLink } from 'vue-router'; 
-  import coverImage from '../assets/images/no-cover.jpg';
+  import noImage from '../assets/images/default-cover.png';
 
   const { filteredCards } = defineProps(['filteredCards']);
 </script>
 
 <template>
   <RouterLink :to="`./category/${quiz.id}`" class="card" v-for="quiz in filteredCards" :key="quiz.id">
-    <div class="card__image-container">
-      <img class="card__image" :src="quiz.img.length ? quiz.img : coverImage" :alt="quiz.name">
+    <div class="card__image-container" :style="{ backgroundImage: `url(${noImage})`}">
+      <img class="card__image" :src="quiz.img" :alt="quiz.name">
     </div>
     <article class="card__info">
       <h2 class="card__title">{{ quiz.name }}</h2>
