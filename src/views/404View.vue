59a27ff470da8a05be5c6a68ca2cf77baaae4ae5
eimwe <prokopenkoserge@inbox.ru>
2023-04-22 14:03:42 +0600

    Changed image at 404 page
    Deleted the previous image from 'assets/images' folder.

--- src/views/404View.vue
+++ src/views/404View.vue
@@ -1,11 +1,11 @@
 <script setup>
   import { RouterLink } from 'vue-router';
-  import coverImage from '../assets/images/no-cover.jpg';
+  import noImage from '../assets/images/default-cover.png';
 </script>
 
 <template>
   <figure class="deadend">
-    <img class="deadend__image" :src="coverImage" alt="Not Found">
+    <img class="deadend__image" :src="noImage" alt="Not Found">
     <figcaption>
       <span class="deadend__title">404 You got lost</span>
       <RouterLink to="/" class="link">Return to the Home page</RouterLink>
