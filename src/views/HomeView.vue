09bee8459573f2867b6818b5abe0c4240252bf15
eimwe <prokopenkoserge@inbox.ru>
2023-07-05 12:57:28 +0600

    Fixed HomeView component issues
    1. Reimported card component as 'QuestionCard.vue';
    2. Rewrote 'filteredCards' computed property to eliminate the side of effect of original array (quizzes) being mutated;
    3. Formatted the code by means of Prettier.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,31 @@
 <script setup>
-  import Card from '../components/Card.vue'
-  import { ref, computed } from 'vue';
-  import loadedQuizData from '../api.js';
+import QuestionCard from '../components/QuestionCard.vue'
+import { ref, computed } from 'vue'
+import loadedQuizData from '../api.js'
 
-  const quizzes = ref(loadedQuizData);
-  const searchQuery = ref('');
+const quizzes = ref(loadedQuizData)
+const searchQuery = ref('')
 
-  const filteredCards = computed(() => {
-    return quizzes.value = loadedQuizData.filter(quiz => quiz.name.toLowerCase().includes(searchQuery.value.toLowerCase()));
-  });
+const filteredCards = computed(() => {
+  return quizzes.value.filter((quiz) =>
+    quiz.name.toLowerCase().includes(searchQuery.value.toLowerCase())
+  )
+})
 </script>
 
 <template>
   <header class="page-header">
     <div class="container">
       <div class="page-header__container">
-        <h1 class="page-header__title">
-          Quizzes
-        </h1>
-        <form
-          class="search"
-          @submit.prevent="$event.preventDefault()"
-          action="#"
-          method="GET"
-        >
-          <input class="search__bar" v-model.trim="searchQuery" type="search" name="search" placeholder="Search">
+        <h1 class="page-header__title">Quizzes</h1>
+        <form class="search" @submit.prevent="$event.preventDefault()" action="#" method="GET">
+          <input
+            class="search__bar"
+            v-model.trim="searchQuery"
+            type="search"
+            name="search"
+            placeholder="Search"
+          />
         </form>
       </div>
     </div>
@@ -32,9 +33,7 @@
   <main class="cards">
     <div class="container">
       <div class="cards__container">
-        <Card
-          :filteredCards="filteredCards"
-        />
+        <QuestionCard :filteredCards="filteredCards" />
       </div>
     </div>
   </main>
