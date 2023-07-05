4c1f1f34909b6ecdc7b8e9a57fdfca6cf06272b3
eimwe <prokopenkoserge@inbox.ru>
2023-07-05 12:53:06 +0600

    Fixed card component issues
    1. Renamed 'Card.vue' into 'QuestionCard.vue' to adopt the multi-word component name rule;
    2. Accepted and used filteredCards prop without destructuring it.

---
+++ src/components/QuestionCard.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+import noImage from '../assets/images/default-cover.png'
+
+const props = defineProps(['filteredCards'])
+</script>
+
+<template>
+  <RouterLink
+    v-for="quiz in props.filteredCards"
+    :key="quiz.id"
+    :to="`./category/${quiz.id}`"
+    class="card"
+  >
+    <div class="card__image-container" :style="{ backgroundImage: `url(${noImage})` }">
+      <img class="card__image" :src="quiz.img" :alt="quiz.name" />
+    </div>
+    <article class="card__info">
+      <h2 class="card__title">{{ quiz.name }}</h2>
+      <span class="card__note">{{ quiz.questions.length }} questions</span>
+    </article>
+  </RouterLink>
+</template>
