603c8089b16783d27284860c9dd75e45cd82d658
eimwe <prokopenkoserge@inbox.ru>
2023-07-06 11:24:38 +0600

    Fixed CategoryView component issues
    1. Reimported card and result components as 'QuizCard.vue' and 'QuizResult.vue';
    2. Formatted code with Prettier.

--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -1,54 +1,56 @@
 <script setup>
-  import { ref, computed } from 'vue';
-  import { useRoute } from 'vue-router';
-  import loadedQuizData from '../api.js';
-  import Question from '../components/Question.vue';
-  import QuestionHeader from '../components/QuestionHeader.vue';
-  import Result from '../components/Result.vue';
-  import NotFound from '../views/404View.vue'
+import { ref, computed } from 'vue'
+import { useRoute } from 'vue-router'
+import loadedQuizData from '../api.js'
+import QuestionContent from '../components/QuestionContent.vue'
+import QuestionHeader from '../components/QuestionHeader.vue'
+import QuizResult from '../components/QuizResult.vue'
+import NotFound from '../views/404View.vue'
 
-  const categoryRoute = useRoute();
-  const category = loadedQuizData.find(category => category.id === parseInt(categoryRoute.params.id));
-  const currentQuestionIndex = ref(0);
-  const numberOfCorrectAnswers = ref(0);
-  const numberOfAnswers = ref([]);
-  const showResults = ref(false);
+const categoryRoute = useRoute()
+const category = loadedQuizData.find(
+  (category) => category.id === parseInt(categoryRoute.params.id)
+)
+const currentQuestionIndex = ref(0)
+const numberOfCorrectAnswers = ref(0)
+const numberOfAnswers = ref([])
+const showResults = ref(false)
 
-  const quizProgress = computed(() => {
-    return `${currentQuestionIndex.value}/${category.questions.length}`;
-  });
+const quizProgress = computed(() => {
+  return `${currentQuestionIndex.value}/${category.questions.length}`
+})
 
-  const barProgress = computed(() => {
-    return parseInt(`${currentQuestionIndex.value / category.questions.length * 100}`).toFixed();
-  });
+const barProgress = computed(() => {
+  return parseInt(`${(currentQuestionIndex.value / category.questions.length) * 100}`).toFixed()
+})
 
-  const onOptionSelected = (isCorrect, option) => {
-    if (isCorrect) {
-      numberOfCorrectAnswers.value++;
-    }
-
-    if ((category.questions.length - 1)  === currentQuestionIndex.value) {
-      showResults.value = true;
-    }
-
-    currentQuestionIndex.value++;
-
-    numberOfAnswers.value.push({
-      id: currentQuestionIndex.value,
-      label: option
-    });
+const onOptionSelected = (isCorrect, option) => {
+  if (isCorrect) {
+    numberOfCorrectAnswers.value++
   }
+
+  if (category.questions.length - 1 === currentQuestionIndex.value) {
+    showResults.value = true
+  }
+
+  currentQuestionIndex.value++
+
+  numberOfAnswers.value.push({
+    id: currentQuestionIndex.value,
+    label: option
+  })
+}
 </script>
 
 <template>
   <div v-if="category">
-    <QuestionHeader :quizProgress="quizProgress" :barProgress="barProgress"/>
-    <Question
+    <QuestionHeader :quizProgress="quizProgress" :barProgress="barProgress" />
+    <QuestionContent
       v-if="!showResults"
       :question="category.questions[currentQuestionIndex]"
       @selectedOption="onOptionSelected"
     />
-    <Result
+    <QuizResult
       v-else
       :numberOfCorrectAnswers="numberOfCorrectAnswers"
       :questionsLength="category.questions.length"
